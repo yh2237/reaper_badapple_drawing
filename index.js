a3886007fe94f6565af07fc745a7a2aadfb95d68
yh <2237.yh.2237@gmail.com>
2025-01-27 23:07:20 +0900

    ミスを修正

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ if (!fs.existsSync(framesDataDir)) fs.mkdirSync(framesDataDir);
 async function processFrame(imagePath, frameNumber) {
     try {
         const frameDataPath = `${framesDataDir}/frame_${frameNumber}.txt`;
-
         const { data, info } = await sharp(imagePath)
             .resize(33, 25) //サイズ（drawing.luaの縦横サイズと同じにする）
             .raw()
@@ -23,7 +22,10 @@ async function processFrame(imagePath, frameNumber) {
         for (let y = 0; y < info.height; y++) {
             for (let x = 0; x < info.width; x++) {
                 const pixelIndex = (y * info.width + x) * info.channels;
-                const brightness = data[pixelIndex];
+                const brightness =
+                    0.2126 * data[pixelIndex] +
+                    0.7152 * data[pixelIndex + 1] +
+                    0.0722 * data[pixelIndex + 2];
                 frameData += brightness > 128 ? "0" : "1";
             }
             frameData += "\n";
@@ -42,11 +44,10 @@ ffmpeg(videoPath)
     .on("end", async () => {
         console.log("フレームの抽出が完了しました！データを生成します");
 
-        const frameFiles = fs.readdirSync(framesDir);
-        for (let i = 0; i < frameFiles.length; i++) {
-            const imagePath = `${framesDir}/${frameFiles[i]}`;
-            await processFrame(imagePath, i + 1);
-        }
+        const frameFiles = fs.readdirSync(framesDir).sort();
+        await Promise.all(
+            frameFiles.map((file, i) => processFrame(`${framesDir}/${file}`, i + 1))
+        );
 
         console.log("すべてのフレームデータが生成されました！");
     })
